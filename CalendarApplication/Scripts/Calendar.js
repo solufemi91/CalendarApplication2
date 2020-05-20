3b1d7be4dfb22014602da67fac47b5f87d59fc77
Steve Olufemi <solufemi-olukanni@hotmail.com>
2020-05-20 18:08:54 +0100

    creation of calender js file and package.json

---
+++ CalendarApplication/Scripts/Calendar.js
@@ -0,0 +1,24 @@
+﻿//import React from 'react';
+//import ReactDOM from 'react-dom';
+
+class CalendarContainer extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            message: "This is my react-calendar"
+        };
+    }
+
+    render(){
+        return (
+            <div>
+                {this.state.message}
+            </div>
+        );
+    }
+}
+
+
+const domContainer = document.querySelector('#root-react');
+ReactDOM.render(<CalendarContainer/>, domContainer);
