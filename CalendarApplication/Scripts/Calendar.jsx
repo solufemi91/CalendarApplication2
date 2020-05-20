7a5cfeb66ce0ad70a1bfb6b6b8945f9c4a621854
Steve Olufemi <solufemi-olukanni@hotmail.com>
2020-05-20 22:49:55 +0100

    first component added

---
+++ CalendarApplication/Scripts/Calendar.jsx
@@ -0,0 +1,21 @@
+﻿class CalendarContainer extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            message: "This is my react-calendar"
+        };
+    }
+
+    render() {
+        return (
+            <div>
+                {this.state.message}
+            </div>
+        );
+    }
+}
+
+
+const domContainer = document.getElementById('content');
+ReactDOM.render(<CalendarContainer />, domContainer); 
