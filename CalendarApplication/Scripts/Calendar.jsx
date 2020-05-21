455143e018f08e5bd42d3913893eaf5da0eadf3f
Steve Olufemi <solufemi-olukanni@hotmail.com>
2020-05-21 01:29:40 +0100

    table header displayed

--- CalendarApplication/Scripts/Calendar.jsx
+++ CalendarApplication/Scripts/Calendar.jsx
@@ -1,21 +1,53 @@
-﻿class CalendarContainer extends React.Component {
+﻿
+class CalendarContainer extends React.Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            message: "This is my react-calendar"
+            message: ""
         };
     }
 
     render() {
         return (
-            <div>
-                {this.state.message}
-            </div>
+            <table>
+                <TableHeader days={this.props.serverData.CalendarData.DaysOfTheWeek}/>
+            </table>
+        );
+    }
+}
+
+class TableHeader extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            daysOfTheWeek: this.props.days
+
+        };
+
+        this.tableHeaderBoxes = this.state.daysOfTheWeek.map((day, index) =>
+            <td className="boxes" key={index}>{day}</td>
+        )
+
+
+
+    }
+
+    render() {
+        return (
+            <thead>
+                <tr>
+                    {this.tableHeaderBoxes}
+                </tr>
+            </thead>
         );
     }
 }
 
 
-const domContainer = document.getElementById('content');
-ReactDOM.render(<CalendarContainer />, domContainer); 
+
+const domContainer = document.getElementById('calendarContainer');
+const data = domContainer.getAttribute('data-react-model');
+const obj = JSON.parse(data);
+ReactDOM.render(<CalendarContainer serverData={obj} />, domContainer); 
