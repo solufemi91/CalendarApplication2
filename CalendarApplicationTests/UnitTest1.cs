f3f7f09108024098943835fe079b1ff5deaba5f8
Steve Olufemi <solufemi-olukanni@hotmail.com>
2021-03-13 17:02:23 +0000

    added  unit test project with no real tests

---
+++ CalendarApplicationTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CalendarApplicationTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void MyTestMethod()
+        {
+        }
+    }
+}
